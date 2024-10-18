5a9aa3b11b6ded0e7333278b4a81a95b4e6fb6ec
fruitninga <ism17v2@gmail.com>
2024-10-18 14:37:04 +0200

    Hacer ejercicio IRPF

--- 04_Formularios/iva_post.php
+++ 04_Formularios/iva_post.php
@@ -29,13 +29,17 @@
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $precio = $_POST["Precio"];
             $iva = $_POST["iva"];
-
-            $pvp = match ($iva) {
-                "General" => $precio * 1.21,
-                "Reducido" => $precio * 1.10,
-                "Superreducido" => $precio * 1.04,
-            };
-            echo "<h3> El PVP es $pvp </h3>";
+            if ($precio != '' and $iva != '') {//control de que me metan algo vacio
+                $pvp = match ($iva) {
+                    "General" => $precio * 1.21,
+                    "Reducido" => $precio * 1.10,
+                    "Superreducido" => $precio * 1.04,
+                };
+                echo "<h3> El PVP es $pvp </h3>";
+            }
+            else {
+                echo "<p>Te faltan datos</p>";
+            }
         }
     ?>
 </body>
