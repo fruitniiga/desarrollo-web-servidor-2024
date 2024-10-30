ff27f6cc561036bb61766b92adb8e7a88dd01ff9
ide-la-r <ism17v2@gmail.com>
2024-10-30 01:11:38 +0100

    Primera parte del ejercio completada

--- 04_Formularios/usuario.php
+++ 04_Formularios/usuario.php
@@ -19,16 +19,55 @@
     <div class="container">
         <?php
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $tmp_dni = $_POST["dni"];
+                $tmp_fechaNacimiento = $_POST["fechaNacimiento"];
+                $tmp_correo = $_POST["correo"];
                 $tmp_usuario = $_POST["usuario"];
                 $tmp_nombre = $_POST["nombre"];
                 $tmp_apellidos = $_POST["apellidos"];
+
+                if ($tmp_dni == '') {
+                    $err_dni = "El DNI es obligatorio";
+                } else{
+                    $patron = "/^[0-9]{8}[A-Z]$/";// el $ y el ^ es para cerrar las expresiones regulares
+                    if (!preg_match($patron, $tmp_dni)) {//esto ve si la variale tmp cumple el patron
+                        $err_dni = "El DNI debe contener 8 numeros y 1 letra mayuscula";
+                    }  else{
+                        $dni = $tmp_dni;
+                    }
+                }
+
+
+                if ($tmp_fechaNacimiento == '') {
+                    $err_fechaNacimiento = "La fecha de nacimiento es obligatoria";
+                } else{
+                    $patron = "/^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/(19|20)\d{2}$/";
+                    if (!preg_match($patron, $tmp_fechaNacimiento)) {
+                        $err_fechaNacimiento = "Debes poner el formato: dd/mm/yyyy";
+                    }  else{
+                        $fechaNacimiento = $tmp_fechaNacimiento;
+                    }
+                }
+
+
+                if ($tmp_correo == '') {
+                    $err_correo = "El correo es obligatorio";
+                } else{
+                    $patron = "/^[^@\\s]+@[^@\\s\\.]+\\.[^@\\s\\.]+$/";
+                    if (!preg_match($patron, $tmp_correo)) {
+                        $err_correo = "Debes poner bien el correo ej: example@gmail.com";
+                    }  else{
+                        $correo = $tmp_correo;
+                    }
+                }
+
 
                 if ($tmp_usuario == '') {
                     $err_usuario = "El usuario es obligatorio";
                 } else{
                     // letras de la A a la Z (mayus o minus), numers y barrabaja
-                    $patron = "/^[a-zA-Z0-9_]{4,12}$/";// el $ y el ^ es para cerrar las expresiones irregulares
-                    if (!preg_match($patron, $tmp_usuario)) {//esto ve si la variale tmp cumple el patron
+                    $patron = "/^[a-zA-Z0-9_]{4,12}$/";
+                    if (!preg_match($patron, $tmp_usuario)) {
                         $err_usuario = "El usuario debe contener de 4 a 12 letras, numeros o barrabaja";
                     }  else{
                         $usuario = $tmp_usuario;
@@ -70,6 +109,21 @@
         ?>
         <h1>Formulario usuario</h1>
         <form class="col-4" action="" method="post"><!-- Esto del col 3 y eso es del bootstrap, cambios del css-->
+            <div class="mb-3">
+                <label class="form-label">DNI</label>
+                <input class="form-control" type="text" name="dni">
+                <?php if(isset($err_dni)) echo "<span class = 'error'>$err_dni</span>" ?>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Fecha de nacimiento</label>
+                <input class="form-control" type="text" name="fechaNacimiento">
+                <?php if(isset($err_fechaNacimiento)) echo "<span class = 'error'>$err_fechaNacimiento</span>" ?>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Correo Electronico</label>
+                <input class="form-control" type="text" name="correo">
+                <?php if(isset($err_correo)) echo "<span class = 'error'>$err_correo</span>" ?>
+            </div>
             <div class="mb-3">
                 <label class="form-label">Usuario</label>
                 <input class="form-control" type="text" name="usuario">
