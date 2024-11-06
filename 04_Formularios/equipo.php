49b2ce3a7f9195a9d04a510494ea59158dd9b56c
ide-la-r <ism17v2@gmail.com>
2024-11-06 11:31:53 +0100

    Hecho una parte de ejercicio videojuego y para hacer el de equipo

---
+++ 04_Formularios/equipo.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio Equipo</title>
+</head>
+<body>
+<!--
+Equipos de la liga
+
+- Nombre (letras con tilde, ñ, espacios en blanco y punto)
+- Inicial (3 letras)
+- Liga (select con las opciones: Liga EA Sports, Liga Hypermotion, Liga Primera RFEF)
+- Ciudad (letras con tilde, ñ, ç y espacios en blanco)
+- Tiene titulo liga (select con si o no)
+- Fecha de fundación (entre hoy y el 18 de diciembre de 1889)
+- Número de jugadores (entre 19 y 32)
+-->
+</body>
+</html>
